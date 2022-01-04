e12b8b9fd7b7876e2dd4591cff447677201ec3d0
Victor <victor.onic@outlook.fr>
2022-01-04 23:06:02 +0100

    Created Cell class

--- src/Cell.cpp
+++ src/Cell.cpp
@@ -1,2 +1,20 @@
 #include "Cell.h"
 
+
+// Constructeur: initialise position.
+Cell::Cell(int row, int col) : position(sf::Vector2i(row, col)), state(Cell::State::BASE) {}
+
+// Obtenir la valeur de position.
+sf::Vector2i Cell::get_position() {
+    return position;
+}
+
+// Obtenir la valeur de state.
+Cell::State Cell::get_state() {
+    return state;
+}
+// Définir la valeur de state.
+void Cell::set_state(Cell::State new_state) {
+    state = new_state;
+}
+
--- src/headers/Cell.h
+++ src/headers/Cell.h
@@ -1,7 +1,20 @@
 #pragma once
-
+#include <SFML/Graphics.hpp>
 
 class Cell {
 
+public:
+    enum class State { BASE, DUG, MINE, FLAG, BOOM };
+
+    Cell(int row, int col);
+
+    sf::Vector2i get_position();
+    State get_state();
+
+    void set_state(State state);
+
+private:
+    sf::Vector2i position;
+    State state;
 
 };
