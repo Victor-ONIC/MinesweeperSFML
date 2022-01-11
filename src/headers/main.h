e1ad4415c2ba9f3fcc0269432f90a8836ba8cc80
Victor <victor.onic@outlook.fr>
2022-01-11 12:13:41 +0100

    Modified constexpr to const

--- src/headers/main.h
+++ src/headers/main.h
@@ -4,21 +4,21 @@
 
 
 // Constantes.
-constexpr int CELL_SIZE = 60;
+const int CELL_SIZE = 60;
 
-constexpr int ROWS = 9;
-constexpr int COLUMNS = 9;
-constexpr int MINES = 10;
+const int ROWS = 9;
+const int COLUMNS = 9;
+const int MINES = 10;
 
-constexpr int RECT_WIDTH = ROWS * CELL_SIZE;
-constexpr int RECT_HEIGHT = 80;
+const int RECT_WIDTH = ROWS * CELL_SIZE;
+const int RECT_HEIGHT = 80;
 
-constexpr int TEXT_SIZE = 60;
-constexpr int MARGIN_X = 10;
-constexpr int MARGIN_Y = 20;
+const int TEXT_SIZE = 60;
+const int MARGIN_X = 10;
+const int MARGIN_Y = 20;
 
-constexpr int MARC_MARGIN_X = 15;
-constexpr int MARC_MARGIN_Y = 10;
+const int MARC_MARGIN_X = 15;
+const int MARC_MARGIN_Y = 10;
 
-constexpr int WIN_WIDTH = ROWS * CELL_SIZE;
-constexpr int WIN_HEIGHT = COLUMNS * CELL_SIZE + RECT_HEIGHT;
+const int WIN_WIDTH = ROWS * CELL_SIZE;
+const int WIN_HEIGHT = COLUMNS * CELL_SIZE + RECT_HEIGHT;
